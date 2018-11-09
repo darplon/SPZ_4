238f83f2c6d2dec0f4ea0be416828279616126a0
Vladyslav <darplon@mail.ru>
2018-11-09 16:48:57 +0200

    Добавьте файлы проекта.

---
+++ SPZ_LB_4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPZ_LB_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
